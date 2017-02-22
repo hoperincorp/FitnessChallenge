ba9bc92841038dcf112afc97f2f7de9705ce6ded
Laszlo Kerrick <laszlokerrick@gmail.com>
2017-02-23 01:18:45 +0300

    Working on FIRST_NOTE

--- app/src/main/java/com/hoperincorp/android/fitnesschallenge/frames/addNoteFrame.java
+++ app/src/main/java/com/hoperincorp/android/fitnesschallenge/frames/addNoteFrame.java
@@ -1,6 +1,7 @@
 package com.hoperincorp.android.fitnesschallenge.frames;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -11,6 +12,59 @@ import android.widget.ImageButton;
 import com.hoperincorp.android.fitnesschallenge.R;
 
 public class addNoteFrame extends AppCompatActivity {
+    private SharedPreferences mSettings;
+
+    //region SET CURRENT PROFILE
+
+    private void setCurrentProfile(String sCurrentProfile) {
+        SharedPreferences.Editor editor = mSettings.edit();
+        switch (sCurrentProfile) {
+            case "FIRST_PROFILE":
+                editor.putInt("CURRENT_PROFILE", 1);
+                editor.apply();
+                break;
+            case "SECOND_PROFILE":
+                editor.putInt("CURRENT_PROFILE", 2);
+                editor.apply();
+                break;
+            case "THIRD_PROFILE":
+                editor.putInt("CURRENT_PROFILE", 3);
+                editor.apply();
+                break;
+            case "FOURTH_PROFILE":
+                editor.putInt("CURRENT_PROFILE", 4);
+                editor.apply();
+                break;
+            case "FIVTH_PROFILE":
+                editor.putInt("CURRENT_PROFILE", 5);
+                editor.apply();
+                break;
+            case "SIXTH_PROFILE":
+                editor.putInt("CURRENT_PROFILE", 6);
+                editor.apply();
+                break;
+            case "SEVENTH_PROFILE":
+                editor.putInt("CURRENT_PROFILE", 7);
+                editor.apply();
+                break;
+            case "EIGHTH_PROFILE":
+                editor.putInt("CURRENT_PROFILE", 8);
+                editor.apply();
+                break;
+        }
+    }
+
+    //endregion
+
+    //region DEBUGG
+
+    private void debugg(String name, int number) {
+        SharedPreferences.Editor editor = mSettings.edit();
+        editor.putInt(name, number);
+        editor.apply();
+    }
+
+    //endregion
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +90,18 @@ public class addNoteFrame extends AppCompatActivity {
                                                   startActivity(intent);
                                               }
                                           }
+        );
+
+        note_first.setOnClickListener(new View.OnClickListener() {
+                                          public void onClick(View v) {
+                                              debugg("FIRST_PROFILE", 0);
+                                              setCurrentProfile("FIRST_NOTE");
+                                              if (mSettings.getInt("FIRST_NOTE", 0) > 0) {
+                                                  Intent intent = new Intent("android.intent.action.note");
+                                                  startActivity(intent);
+                                              }
+                                          }
+                                      }
         );
     }
 }
